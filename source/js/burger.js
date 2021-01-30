a919a1539943161b07f0507d07f85dec2f1a74ba
el ganso <elganso45@gmail.com>
2021-01-30 14:27:58 +0300

    commit

--- source/js/burger.js
+++ source/js/burger.js
@@ -2,17 +2,15 @@
 let burger = document.querySelector(".burger");
 let active = document.querySelector(".active");
 let wrapper = document.querySelector(".wrapper");
-let b1 = document.querySelector('.b1')
-let b2 = document.querySelector('.b2')
-let b3 = document.querySelector('.b3')
+let b1 = document.querySelector(".b1");
+let b2 = document.querySelector(".b2");
+let b3 = document.querySelector(".b3");
 
 
-burger.addEventListener("click", f1);
-
-function f1() {
+burger.addEventListener("click", () => {
   wrapper.classList.toggle("active");
-  b1.classList.toggle('active')
-  b2.classList.toggle('active')
-  b3.classList.toggle('active')
+  b1.classList.toggle("active");
+  b2.classList.toggle("active");
+  b3.classList.toggle("active");
+});
 
-}
--- source/js/modal.js
+++ source/js/modal.js
@@ -2,16 +2,22 @@ let cross = document.querySelector(".cross-modal-downloads");
 let modalDowload = document.querySelector(".modal__dowload");
 let files = document.querySelector(".files");
 let amd = document.querySelector(".active-modal-dowload");
+let body = document.querySelector("body");
+// let wrapper = document.querySelector(".wrapper");
 
 cross.addEventListener("click", f1);
 files.addEventListener("click", f2);
 
 function f1() {
   modalDowload.classList.remove("active-modal-dowload");
+  // body.classList.remove("modal-active");
 }
 function f2() {
   // modalDowload.style.display = "block";
   modalDowload.classList.add("active-modal-dowload");
+    wrapper.classList.toggle("active");
+    b1.classList.toggle("active");
+    b2.classList.toggle("active");
+    b3.classList.toggle("active");
+  // body.classList.add("modal-active");
 }
-
-
