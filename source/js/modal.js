b4d72eae1d199f12282993835809487ae6c8db60
el ganso <elganso45@gmail.com>
2021-01-31 13:03:34 +0300

    subnav

--- source/js/modal.js
+++ source/js/modal.js
@@ -2,15 +2,12 @@ let cross = document.querySelector(".cross-modal-downloads");
 let modalDowload = document.querySelector(".modal__dowload");
 let files = document.querySelector(".files");
 let amd = document.querySelector(".active-modal-dowload");
-let body = document.querySelector("body");
-// let wrapper = document.querySelector(".wrapper");
 
 cross.addEventListener("click", f1);
 files.addEventListener("click", f2);
 
 function f1() {
   modalDowload.classList.remove("active-modal-dowload");
-  // body.classList.remove("modal-active");
 }
 function f2() {
   // modalDowload.style.display = "block";
@@ -18,6 +15,5 @@ function f2() {
     wrapper.classList.toggle("active");
     b1.classList.toggle("active");
     b2.classList.toggle("active");
-    b3.classList.toggle("active");
-  // body.classList.add("modal-active");
+  b3.classList.toggle("active");
 }
