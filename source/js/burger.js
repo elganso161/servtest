fd98d90ba74dd9ba62195e45d29ca2fff0dcbcb1
el ganso <elganso45@gmail.com>
2021-02-02 18:06:19 +0300

    commit

--- source/js/burger.js
+++ source/js/burger.js
@@ -6,11 +6,11 @@ let b1 = document.querySelector(".b1");
 let b2 = document.querySelector(".b2");
 let b3 = document.querySelector(".b3");
 
-
 burger.addEventListener("click", () => {
   wrapper.classList.toggle("active");
   b1.classList.toggle("active");
   b2.classList.toggle("active");
   b3.classList.toggle("active");
+  
+  body.classList.toggle("body-active");
 });
-
--- source/js/modal.js
+++ source/js/modal.js
@@ -4,13 +4,19 @@ let files = document.querySelector(".files");
 let amd = document.querySelector(".active-modal-download");
 let modal = document.querySelector(".modal");
 
+let body = document.querySelector("body");
+
 cross.addEventListener("click", f1);
 files.addEventListener("click", f2);
 
 function f1() {
   modalDownload.classList.remove("active-modal-download");
   modal.classList.remove("modal-active");
+
+  // body.style.overflow = "visible";
+  body.classList.toggle("body-active");
 }
+
 function f2() {
   modalDownload.classList.add("active-modal-download");
   wrapper.classList.toggle("active");
@@ -18,4 +24,7 @@ function f2() {
   b2.classList.toggle("active");
   b3.classList.toggle("active");
   modal.classList.add("modal-active");
+
+  // body.style.overflow = "hidden";
+  body.classList.toggle("body-active");
 }
