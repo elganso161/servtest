29a96e747be811fd409f06e3854f4074f1d281e9
el ganso <elganso45@gmail.com>
2021-02-09 15:15:07 +0300

    slider

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,26 +1,13 @@
-// var swiper = new Swiper(".blog-slider", {
-//   spaceBetween: 30,
-//   effect: "fade",
-//   loop: true,
-//   mousewheel: {
-//     invert: false,
-//   },
-//   // autoHeight: true,
-//   pagination: {
-//     el: ".blog-slider__pagination",
-//     clickable: true,
-//   },
-// });
-
-
-
-
-    var swiper = new Swiper(".swiper-container", {
-      speed: 600,
+    let swiper = new Swiper(".swiper-container", {
+      speed: 1000,
       parallax: true,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
       },
       navigation: {
         nextEl: ".swiper-button-next",
